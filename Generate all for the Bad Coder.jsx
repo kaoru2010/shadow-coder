bb56908d1731c637ff07f93f481d1ff32954ca1a
kaoru2010 <k.yanase.2009@gmail.com>
2016-02-01 00:12:18 +0900

    Added single and all-versions.

--- Generate all for the Bad Coder.jsx
+++ Generate all for the Bad Coder.jsx
@@ -11,15 +11,15 @@ function exportFileToSVG(doc, dest) {
 (function() {
     var i, doc, docPath, svgPath;
 
-    if (0 < app.documents.length) {
-        doc = app.activeDocument;
-        if (doc.name.indexOf('名称未設定') >= 0) return;
+    for (i = 0; i < app.documents.length; i++) {
+        doc = app.documents[i];
+        if (doc.name.indexOf('名称未設定') >= 0) continue;
 
-        if (doc.fullName == null || doc.fullName.fsName.indexOf('/documents/') == -1) return;
+        if (doc.fullName == null || doc.fullName.fsName.indexOf('documents') == -1) continue;
 
         docPath = doc.fullName.fsName;
         svgPath = docPath.replace(/\.ai$/, ".svg");
-        if (docPath == svgPath) return;
+        if (docPath == svgPath) continue;
 
         exportFileToSVG(doc, svgPath);
         doc.saveAs(new File(docPath));
